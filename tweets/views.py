091aed6574fab54b7599b5af8aed761a48d8f43d
Eliud Kagema <eliud.k.maina@gmail.com>
2020-08-20 20:58:13 +0300

    creating api views

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
 
-# Create your views here.
+def index(request, *args, **kwargs):
+    context = {}
+    template_name = 'index.html'
+    return render(request, template_name, context)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+    context = {
+        "id": tweet_id,
+        "content": obj.content,
+        # "image_path": obj.image.url,
+
+    }
+    template_name = 'index.html'
+    return JsonResponse(context)
